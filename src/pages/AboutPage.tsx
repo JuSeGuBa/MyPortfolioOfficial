71d30d25ac6aba804b94faf59fa941eb5e18813f
sebasps532@gmail.com <you@example.comsebasps532@gmail.com>
2024-11-06 14:20:12 -0500

    set routes

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,9 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>SobreMi</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ src/routes/root.tsx
@@ -0,0 +1,28 @@
+import HomePage from "../pages/HomePage";
+import AboutPage from "../pages/AboutPage";
+import ProjectsPage from "../pages/ProjectsPage";
+import ContactPage from "../pages/ContactPage";
+import {createBrowserRouter,} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+    {
+      path: "/about",
+      element: <AboutPage />,
+    },
+    {
+        path: "/projects",
+        element: <ProjectsPage />,
+    },
+    {
+        path: "/contact",
+        element: <ContactPage />
+    }
+  ]);
+
+
+  export default router;
