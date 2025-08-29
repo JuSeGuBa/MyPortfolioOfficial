0f8933056ea6484cc016fb768220161510d0ec3f
sebasps532@gmail.com <you@example.comsebasps532@gmail.com>
2025-08-29 17:55:30 -0500

    fondo movimiento

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import router from "./routes/root";
+import Background from "./components/Background";
 
 function App() {
   return (
     <div>
+      <Background />
       <RouterProvider router={router} />
     </div>
   );
---
+++ src/components/Background.tsx
@@ -0,0 +1,18 @@
+import { motion } from "framer-motion";
+
+export default function Background() {
+  return (
+    <motion.img
+      src="/fondo/fondo6.png "
+      alt="Fondo animado"
+      initial={{ scale: 1 }}
+      animate={{ scale: [1, 1.03, 1] }} // movimiento hacia adelante y atrás
+      transition={{
+        duration: 10, // tiempo total de un ciclo
+        repeat: Infinity, // repetir siempre
+        ease: "easeInOut", // suavidad natural
+      }}
+      className="fixed inset-0 -z-10 w-full h-auto object-center"
+    />
+  );
+}
