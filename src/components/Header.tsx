4b13032ef32a1f5a1bfd982e09024d87b978c6d1
SebasGuz <sebastianguzman0114@gmail.com>
2026-01-22 09:59:53 -0500

    new scroll in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,46 +1,32 @@
 import "../styles/Header.css";
-import { IoArrowDownSharp } from "react-icons/io5";
 import { useEffect, useState } from "react";
 
 const Header = () => {
   const [windowHeight, setWindowHeight] = useState("100vh");
 
   useEffect(() => {
-    const setVH = () => {
-      setWindowHeight(`${window.innerHeight}px`);
-    };
-
+    const setVH = () => setWindowHeight(`${window.innerHeight}px`);
     setVH();
     window.addEventListener("resize", setVH);
     window.addEventListener("orientationchange", setVH);
-
     return () => {
       window.removeEventListener("resize", setVH);
       window.removeEventListener("orientationchange", setVH);
     };
   }, []);
 
-  const scrollToNextSection = () => {
-    const nextSection = document.getElementById("about-section");
-    if (nextSection) {
-      nextSection.scrollIntoView({ behavior: "smooth" });
-    }
-  };
-
   return (
-    <header
-      id="headerMe"
-      className="header-container"
-      style={{ height: windowHeight }}
-    >
+    <header className="header-container" style={{ height: windowHeight }}>
       <img
         src={`${import.meta.env.BASE_URL}lego2.jpg`}
         alt="FrontendIMG"
         className="imageFrontened"
       />
-      <button className="button-arrow" onClick={scrollToNextSection}>
-        <IoArrowDownSharp />
-      </button>
+
+      <div className="scroll-indicator">
+        <span className="scroll-line" />
+        <span className="scroll-arrow" />
+      </div>
     </header>
   );
 };
